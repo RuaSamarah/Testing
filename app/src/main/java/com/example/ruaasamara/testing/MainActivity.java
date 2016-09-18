d75f76253b09a140f1801208eb2e83a939cf9599
ansam <ansam.soliman@gmail.com>
2016-09-18 16:00:29 +0300

    adding test funvtion to branch01 from ansam.......

--- app/src/main/java/com/example/ruaasamara/testing/MainActivity.java
+++ app/src/main/java/com/example/ruaasamara/testing/MainActivity.java
@@ -47,11 +47,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    void testing5() {
-        int x = 2;
-        int y = 4;
-        int z = x+y;
-    }
+
     void testing6() {
         int x = 2;
         int y = 4;
