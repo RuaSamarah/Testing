e736829b15ab49f0702d89d8f1953b744ce9afc8
RUA'A SAMARA <ruaasamara@RUAAs-MacBook-Pro.local>
2016-09-18 11:02:26 +0300

    aded a new method called testing

--- app/src/main/java/com/example/ruaasamara/testing/MainActivity.java
+++ app/src/main/java/com/example/ruaasamara/testing/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    void testing() {
+        int x = 2;
+        int y = 4;
+        int z = x+y;
+    }
+
 }
